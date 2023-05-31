d846b1246517d5b1a94c2acd840ee4888ddf15fa
GTiko <gemechutiko@gmail.com>
2023-05-30 23:49:42 -0500

    move the project

--- final-project/Jest/cloneArray.test.js
+++
@@ -1,7 +0,0 @@
-const cloneArray = require("./cloneArray");
-
-test("It clone properly", ()=>{
-    const x = [1,2,3]
-    expect(cloneArray(x)).toEqual(x)
-    expect(cloneArray(x)).not.toBe(x)
-})
--- final-project/project-GTiko/src/Frontend/routers/ErrorPage.jsx
+++
@@ -1,7 +0,0 @@
-export function ErrorPage() {
-  return (
-    <center>
-      <h1>Error... 404 Page Not found</h1>
-    </center>
-  );
-}
